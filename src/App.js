9329a9d8900d6f0d6f2f0576df4432d105913467
wellsousaaa <wendell25sousaferreira@gmail.com>
2022-05-31 23:22:45 -0300

    Add electron

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import { initialKeys } from "./helpers/keys";
+import Keyboard from "react-simple-keyboard";
+import "./styles/keyboard.css";
+const ipcRenderer = window.require("electron").ipcRenderer;
+
+function App() {
+  const keyboard = useRef();
+  const [keys, setKeys] = useState(initialKeys);
+
+  const onKeyPress = async (button) => {
+    console.log("Button pressed", button);
+    const file = await ipcRenderer.invoke("open-file");
+    console.log(file);
+  };
+
+  useEffect(() => {
+    keyboard.current.recurseButtons((buttonElement) => {
+      console.log("buttonElement", buttonElement);
+      buttonElement.dataset.value = keys[buttonElement.dataset.skbtn];
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Keyboard
+        keyboardRef={(r) => (keyboard.current = r)}
+        onKeyPress={onKeyPress}
+      />
+    </div>
+  );
+}
+
+export default App;
